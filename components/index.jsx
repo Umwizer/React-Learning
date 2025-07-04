5c0373951313be771524bcfd3ba556d5b7cbadc6
Umwizer <ruthumwizerwa@gmail.com>
2025-07-04 11:27:44 +0200

    components

---
+++ components/index.jsx
@@ -0,0 +1,12 @@
+function MyButton() {
+  return <button>I'm button</button>;
+}
+
+export default function myApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
