1f6ff566b1d06ec112ac818e2e66e47c9edc1ae8
Umwizer <ruthumwizerwa@gmail.com>
2025-07-07 11:33:34 +0200

    bootstrap install and components creation

--- Mosh Course/react-app/src/Message.tsx
+++ Mosh Course/react-app/src/Message.tsx
@@ -1,7 +1,9 @@
 //PascalCasing
 function Message() {
   //JSX : Javascript XML
-  return <h1>Hello World</h1>;
+  const name = "Ruth";
+  if (name) return <h1>Hello, {name} </h1>;
+  return <h1>Hello , World</h1>;
 }
 
 export default Message;
