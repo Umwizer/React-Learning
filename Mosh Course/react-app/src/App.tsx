96e1953a3f2950d0d5392bb8472735f0f2842fc2
Umwizer <ruthumwizerwa@gmail.com>
2025-07-09 09:17:05 +0200

    building components

--- Mosh Course/react-app/src/App.tsx
+++ Mosh Course/react-app/src/App.tsx
@@ -1,16 +1,27 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
+// function App() {
+//   let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+//   const handleSelectItem = (item: string) => {
+//     console.log(item);
+//   };
+//   return (
+//     <div>
+//       <ListGroup
+//         items={items}
+//         heading="Cities"
+//         onSelectItem={handleSelectItem}
+//       />
+//     </div>
+//   );
+// }
+// export default App;
+
+import Alert from "./components/Alert";
+
 function App() {
-  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>"Hello World" </Alert>
     </div>
   );
 }
