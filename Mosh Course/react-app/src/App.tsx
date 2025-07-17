2eda17486aca8090b7cc80e3eeac464d00d2e1cf
Umwizer <ruthumwizerwa@gmail.com>
2025-07-17 09:40:32 +0200

    assign

--- Mosh Course/react-app/src/App.tsx
+++ Mosh Course/react-app/src/App.tsx
@@ -16,31 +16,77 @@
 // }
 // export default App;
 
-// import { useState } from "react";
-// import Alert from "./components/Alert";
-// import Button from "./components/Button";
-// function App() {
-//   const [person, setPerson] = useState({
-//     firtName: "",
-//     lastName: "",
-//     contact: {
-//       address: {
-//         street: "",
-//       },
-//     },
-//   });
-//   const [isLoading, setLoading] = useState(false);
-//   return <div></div>;
-// }
-// export default App;
-
-import Message from "./Message";
-
+import { useState } from "react";
 function App() {
+  const [customer, setCustomer] = useState({
+    name: "Ruth",
+    contact: {
+      address: {
+        city: "San Francisco",
+        zipCode: 9411,
+      },
+    },
+  });
+
+  const handleClick = () => {
+    setCustomer({
+      ...customer,
+      contact: {
+        ...customer.contact,
+        address: {
+          ...customer.contact.address,
+          zipCode: 94112,
+        },
+      },
+    });
+  };
   return (
     <div>
-      <Message />
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
 export default App;
+
+// import { useState } from "react";
+
+// // function App() {
+// //   const [drink, setDrink] = useState({
+// //     title: "Americano",
+// //     price: 5,
+// //   });
+// //   const handleClick = () => {
+// //     setDrink({ ...drink, price: 6 });
+// //   };
+// //   return (
+// //     <div>
+// //       <button onClick={handleClick}>Click Me</button>
+// //     </div>
+// //   );
+// // }
+// // export default App;
+
+//Updating Arrays
+function Apps() {
+  const [tags, setTags] = useState(["happy", "cheerful"]);
+  const handleClick = () => {
+    //Add
+    setTags([...tags, "exciting"]);
+    //Remve
+    setTags(tags.filter((tag) => tag !== "happy"));
+    //Remove
+    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
+}
+//updating objects
+function App1() {
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug1", fixed: false },
+    { id: 2, title: "Bugs2", fixed: false },
+  ]);
+}
