ad58c37d5fdc36c8a62b921870160cd9de1bc5a8
Umwizer <ruthumwizerwa@gmail.com>
2025-07-21 10:41:26 +0200

    creating forms in react

--- Mosh Course/react-app/src/App.tsx
+++ Mosh Course/react-app/src/App.tsx
@@ -1,95 +1,38 @@
-// import ListGroup from "./components/ListGroup";
-// function App() {
-//   let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-//   const handleSelectItem = (item: string) => {
-//     console.log(item);
+// //updating objects
+// import { useState } from "react";
+// import produce from "immer";
+
+// function App1() {
+//   const [bugs, setBugs] = useState([
+//     { id: 1, title: "Bug1", fixed: false },
+//     { id: 2, title: "Bugs2", fixed: false },
+//   ]);
+//   const handleClick = () => {
+//     setBugs(
+//       produce((draft) => {
+//         const bug = draft.find((bug) => bug.id === 1);
+//         if (bug) bug.fixed = true;
+//       })
+//     );
 //   };
 //   return (
 //     <div>
-//       <ListGroup
-//         items={items}
-//         heading="Cities"
-//         onSelectItem={handleSelectItem}
-//       />
+//       {bugs.map((bug) => (
+//         <p key={bug.id}>
+//           {bug.title} {bug.fixed ? "Fixed" : "New"}
+//         </p>
+//       ))}
+//       <button onClick={handleClick}>Click Me</button>
 //     </div>
 //   );
 // }
-// export default App;
-
-import { useState } from "react";
+// export default App1;
+import Form from "./components/form";
 function App() {
-  const [customer, setCustomer] = useState({
-    name: "Ruth",
-    contact: {
-      address: {
-        city: "San Francisco",
-        zipCode: 9411,
-      },
-    },
-  });
-
-  const handleClick = () => {
-    setCustomer({
-      ...customer,
-      contact: {
-        ...customer.contact,
-        address: {
-          ...customer.contact.address,
-          zipCode: 94112,
-        },
-      },
-    });
-  };
   return (
     <div>
-      <button onClick={handleClick}>Click Me</button>
+      <Form />
     </div>
   );
 }
 export default App;
-
-// import { useState } from "react";
-
-// // function App() {
-// //   const [drink, setDrink] = useState({
-// //     title: "Americano",
-// //     price: 5,
-// //   });
-// //   const handleClick = () => {
-// //     setDrink({ ...drink, price: 6 });
-// //   };
-// //   return (
-// //     <div>
-// //       <button onClick={handleClick}>Click Me</button>
-// //     </div>
-// //   );
-// // }
-// // export default App;
-
-//Updating Arrays
-function Apps() {
-  const [tags, setTags] = useState(["happy", "cheerful"]);
-  const handleClick = () => {
-    //Add
-    setTags([...tags, "exciting"]);
-    //Remve
-    setTags(tags.filter((tag) => tag !== "happy"));
-    //Remove
-    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
-  };
-  return (
-    <div>
-      <button onClick={handleClick}>Click Me</button>
-    </div>
-  );
-}
-//updating objects
-function App1() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug1", fixed: false },
-    { id: 2, title: "Bugs2", fixed: false },
-  ]);
-  const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-  };
-}
