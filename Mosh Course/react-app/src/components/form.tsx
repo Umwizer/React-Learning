a6fd63164b2d0059ec13d5b3762fc3212a7b09ca
Umwizer <ruthumwizerwa@gmail.com>
2025-07-21 11:43:40 +0200

    using state

--- Mosh Course/react-app/src/components/form.tsx
+++ Mosh Course/react-app/src/components/form.tsx
@@ -1,9 +1,13 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
 
 const Form = () => {
+  const [person, setPerson] = useState({
+    name: "",
+    age: 0,
+  });
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Submitted");
+    console.log(person);
   };
   return (
     <form action="" onSubmit={handleSubmit}>
@@ -11,13 +15,27 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-controller" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          id="age"
+          type="number"
+          className="form-controller"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
