33327ea1c6e4cdd9efd24262929f1ac0060c41a9
Umwizer <ruthumwizerwa@gmail.com>
2025-08-19 12:42:25 +0200

    creating tailwind css environment

--- Mosh Course/react-app/src/App.tsx
+++ Mosh Course/react-app/src/App.tsx
@@ -1,11 +1,14 @@
 // import CheckPasswordStrenght from "./components/MentalModel/CheckPasswordStrenght";
 import CartShopping from "./components/MentalModel/CartShopping";
-
+import DarkMode from "./components/MentalModel/DarkMode";
+import "./index.css";
 function App() {
   return (
     <div>
-      {/* <CheckPasswordStrenght /> */}
-      <CartShopping />
+      {/* <CheckPasswordStrenght /> *
+      /}
+      {/* <CartShopping /> */}
+      <DarkMode />
     </div>
   );
 }
---
+++ Mosh Course/react-app/src/components/MentalModel/DarkMode.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+const DarkMode = () => {
+  return (
+    <div className="min-h-screen  flex items-center justify-center bg-gray-100">
+      <div></div>
+    </div>
+  );
+};
+
+export default DarkMode;
