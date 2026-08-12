d099ce0917f4eed244db27f13a073bbc0e1a91b6
Umwizer <ruthumwizerwa@gmail.com>
2026-08-12 14:47:28 +0200

    props

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,20 +1,16 @@
 
 import { useState } from 'react';
+import BlogList from './Blog';
 const Home = ()=>{
-    const [blogs,setBlogs] = useState([
+    const [blogs] = useState([
         {title:'My new Website',body:'lorem ipsum...' ,author:'Ruth',id:1},
         {title:'Welcome Party!',body:'lorem ipsum...',author:'Rene',id:2},
-        {title:'Web dev Top Tips',body:'lorem ipsum...',author:'Sandra',id:3}
+        {title:'Web dev Top Tips',body:'lorem ipsum...',author:'Ruth',id:3}
     ])
  return (
     <div className="home">
-        {blogs.map((blog)=>(
-            <div className="blog-preview" key={blog.id}>
-               <h2>{blog.title}</h2>
-               <p>written byt { blog.author }</p>
-            </div>
-        ))}
-        
+        <BlogList blogs={blogs} title ="All Blog" />
+        <BlogList blogs={blogs.filter((blog)=> blog.author === 'Ruth')} title ="Ruth's Blogs"/>
     </div>
  )
 }
