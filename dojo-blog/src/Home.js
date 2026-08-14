7afdad933186960d16e070d3a9b07252246896e1
Umwizer <ruthumwizerwa@gmail.com>
2026-08-14 12:24:22 +0200

    functions as props

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,15 +2,19 @@
 import { useState } from 'react';
 import BlogList from './Blog';
 const Home = ()=>{
-    const [blogs] = useState([
+    const [blogs,setBlogs] = useState([
         {title:'My new Website',body:'lorem ipsum...' ,author:'Ruth',id:1},
         {title:'Welcome Party!',body:'lorem ipsum...',author:'Rene',id:2},
         {title:'Web dev Top Tips',body:'lorem ipsum...',author:'Ruth',id:3}
     ])
+const handleDelete = (id) =>{
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+}
  return (
     <div className="home">
-        <BlogList blogs={blogs} title ="All Blog" />
-        <BlogList blogs={blogs.filter((blog)=> blog.author === 'Ruth')} title ="Ruth's Blogs"/>
+        <BlogList blogs={blogs} title ="All Blog" handleDelete={handleDelete} />
+        <BlogList blogs={blogs.filter((blog)=> blog.author === 'Ruth')} title ="Ruth's Blogs" handleDelete={handleDelete}/>
     </div>
  )
 }
