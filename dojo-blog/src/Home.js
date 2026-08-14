5606ce1713761bc2123e790290e8cb18a3471514
Umwizer <ruthumwizerwa@gmail.com>
2026-08-14 15:13:58 +0200

    Conditional Loading Message

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,25 +2,26 @@
 import { useState ,useEffect} from 'react';
 import BlogList from './Blog';
 const Home = ()=>{
-    const [blogs,setBlogs] = useState(null)
-const [name,setName] = useState('Ruth');
-
+const [blogs,setBlogs] = useState(null)
+const [isPending,setIsPending] = useState(true);
 useEffect(()=>{
-   fetch('http://localhost:8000/blogs')
+  setTimeout(()=>{
+    fetch('http://localhost:8000/blogs')
    .then(res =>{
     return res.json();
    })
    .then(data=>{
-    console.log(data);
     setBlogs(data)
-   })
+    setIsPending(false);
+  });
+   },1000)
 },[]);
  return (
     <div className="home">
+        {isPending && <div>Loading...</div>}
       { blogs &&  <BlogList blogs={blogs} title ="All Blog"  />}
        {/* { blogs && <BlogList blogs={blogs.filter((blog)=> blog.author === 'Ruth')} title ="Ruth's Blogs" handleDelete={handleDelete}/>} */}
        {/* {blogs && <button onClick ={()=>setName('Rene')}>Change Name</button>} */}
-        <p>{name}</p>
     </div>
  )
 }
